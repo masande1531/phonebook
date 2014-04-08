85be2996b946577b2b6ae4fd6068315de94ae60f
masande1531 <masande@globalimsa.com>
2014-04-08 14:24:09 +0200

    Create init.php
    call the class phonebook

---
+++ init.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author Masande
+ * Create 08-Aoril-2014
+ */
+
+require_once ('class/phonebook.php'); 
+
+$object = new phonebook();
+?>
